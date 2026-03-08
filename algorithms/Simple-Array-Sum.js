b01c9292e0a61c5558d00594d94136c5baf10d5d
Muhammad Bilal <muhammad.bilal125150@gmail.com>
2026-03-09 00:55:35 +0500

    Refactor simpleArraySum function and clean up code
    Removed unused variable declarations and adjusted formatting.

--- algorithms/Simple-Array-Sum.js
+++ algorithms/Simple-Array-Sum.js
@@ -2,10 +2,9 @@
 // Platform: HackerRank
 
 function simpleArraySum(ar){
+    let sum = 0;
     for(let i=0; i<ar.length; i++){
      sum += ar[i];
     }
     return sum;
 }
-     let ar = [1,2,3];
-     let sum = 0;

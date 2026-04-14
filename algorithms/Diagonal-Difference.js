ecb29f1680d7ad092042ee850feb0f30145c7114
Muhammad Bilal <muhammad.bilal125150@gmail.com>
2026-04-14 22:03:43 +0500

    Implement diagonalDifference function for array

---
+++ algorithms/Diagonal-Difference.js
@@ -0,0 +1,13 @@
+// Problem: Diagonal Difference
+// Platform: HackerRank
+
+function diagonalDifference(arr) {
+let primary = 0;
+let secondary = 0;
+for(let i=0; i<arr.length; i++){
+    primary += arr[i][i];
+    secondary += arr[i][arr.length-1-i];
+}
+    return Math.abs(primary-secondary);
+
+}
